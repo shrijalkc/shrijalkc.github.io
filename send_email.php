516f18e10f82ec1778751c8d1b161dd328eee85b
Shrijal KC <132465511+shrijalkc@users.noreply.github.com>
2024-08-31 12:29:49 -0400

    Added bootstrap burger menu
    need to add images to comm, ind, res pages

---
+++ send_email.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars(trim($_POST['name']));
+    $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+    $message = htmlspecialchars(trim($_POST['message']));
+
+    $to = "smoxyfox1@gmail.com";
+    $subject = "Contact Form Submission from $name";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "There was a problem sending the message.";
+    }
+} else {
+    echo "Invalid request method.";
+}
+?>
